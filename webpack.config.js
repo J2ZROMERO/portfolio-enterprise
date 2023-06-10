4dea8742fc6cf342ea980deb048d88fb09e02668
Jose Zepeda <jose-zromero@hotmail.com>
2023-06-09 19:49:55 -0600

    Add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,33 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
+        filename: '[name].[contenthash].js',
+        clean: true
     },
-module: {
-    rules: [
-        {
-            test: /\.scss$/,
-            use: ['style-loader','css-loader','sass-loader']   
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
         },
-    ]
-},
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+        watchFiles: ['src/*.html', 'src/**/*.scss', 'src/*.js']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', // 3. Inject styles into DOM
+                    'css-loader', // 2. Turns css into commonjs
+                    'sass-loader' // 1. Turns sass into css
+                ]
+            },
+        ],
+      },
 plugins: [
     new HtmlWebpackPlugin({
         title: 'Enterprise',
